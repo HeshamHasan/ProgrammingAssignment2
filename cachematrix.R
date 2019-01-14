fc127d6b9e7bb2312028b216294ecd932dd4c667
HeshamHasan <45587947+HeshamHasan@users.noreply.github.com>
2019-01-14 15:43:06 +0400

    Add files via upload
    Completed assignment

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## These two functions can be used to make special matrix that caches the inverse of a square
+## matrix, then get it from the cached data if the matrix is unganged
+
+## This function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
+
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## above. If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+}
